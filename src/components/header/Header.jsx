665d4d0371cfe9e16847fe1f07f77a9357cc4880
joao <joaopedrot1912@gmail.com>
2024-08-19 14:35:40 -0300

    feat: protótipo do header e nav adicionado

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <header>
+        <div className='container-logo'>
+            <img className='imagem-logo' alt='Logo' src='./logo-grande.png'/>
+        </div>
+        <div className='container-search-input'>
+            <input placeholder='O que você procura ...'/>
+        </div>
+        <div className='container-botao-nav'>
+            <button>
+                <img alt='Imagem do botão perfil' src='./perfil.png' className='botao-perfil'/>
+            </button>
+            <button>
+                <img alt='Imagem do botão carrinho' src='./carrinho.png' className='botao-carrinho'/>
+            </button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <nav>
+        <button>botao</button>
+        <button>botao</button>
+        <button>botao</button>
+        <button>botao</button>
+    </nav>
+  )
+}
+
+export default Nav
