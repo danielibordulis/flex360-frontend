09108813a5d10d1be8ff2683a0a4376b33ad3eb4
Joao Pedro <joaopedrot1912@gmail.com>
2024-08-21 19:27:06 -0300

    feat: estrutura das paginas

---
+++ src/components/pages/pagCadeiras/PagCadeiras.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../header/Header'
+import Nav from '../../nav/Nav'
+import "./PagCadeiras.css"
+
+function PagCadeiras() {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <section className='body'>
+            
+        </section>
+        <Footer/>
+    </>
+  )
+}
+
+export default PagCadeiras
