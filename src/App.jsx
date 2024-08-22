0bbf7bca2704de5f07638a46c5c6cff97c9f66b5
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-08-22 15:04:26 -0300

    desenho da tela login esta pronto

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import PagLogin from './components/pages/pagLogin/PagLogin'
 
 function App() {
   
 
   return (
     <>
-      <Header />
-      <Nav />
+      <PagLogin/>
     </>
   )
 }
