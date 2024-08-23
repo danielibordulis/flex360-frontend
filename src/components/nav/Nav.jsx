1ea567561ec9ba8815b6f78c4c35a0a1ff2df241
Joao Pedro <joaopedrot1912@gmail.com>
2024-08-23 15:28:27 -0300

    feat: responsividade do nav e header

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,10 +4,13 @@ import "./Nav.css"
 function Nav() {
   return (
     <nav>
+        <button>Home</button>
+        <button>Cadeiras</button>
+        <button>Sugestão Ergonomia</button>
         <button>botao</button>
-        <button>botao</button>
-        <button>botao</button>
-        <button>botao</button>
+        <button className='button-menu'>
+          <img src='./hamb.png' alt='Botão Menu'/>
+        </button>
     </nav>
   )
 }
