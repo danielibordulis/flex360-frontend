2e0c2048178df17994cd5ee369292219d83c0f7c
Joao Pedro <joaopedrot1912@gmail.com>
2024-08-23 16:12:55 -0300

    feat: dropDown do nav funcionando

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Nav.css"
 
 function Nav() {
+
+  const [dropDown, setDropDown] = useState(false)
+
   return (
-    <nav>
-        <button>Home</button>
-        <button>Cadeiras</button>
-        <button>Sugestão Ergonomia</button>
-        <button>botao</button>
-        <button className='button-menu'>
-          <img src='./hamb.png' alt='Botão Menu'/>
+    <nav className={dropDown ? "dropDownNav" : ""}>
+        <button 
+          className='button-menu'
+          onClick={() => {setDropDown(!dropDown)}} 
+        >
+          <img src={dropDown ? './x.png' : './hamb.png'} alt='Botão Menu' style={{width: dropDown ? "27px" : "35px"}}/>
         </button>
+        <button className={dropDown ? "dropDownButton" : ""}>Home</button>
+        <button className={dropDown ? "dropDownButton" : ""}>Cadeiras</button>
+        <button className={dropDown ? "dropDownButton" : ""}>Sugestão Ergonomia</button>
+        <button className={dropDown ? "dropDownButton" : ""}>botao</button>
     </nav>
   )
 }
