3370eed203c478fcf520554adbde62a8f29ed233
Danieli <danielibordulis@gmail.com>
2024-08-23 21:39:41 -0300

    tela do home feita e componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import PagHome from './components/pages/pagHome/PagHome'
 
 function App() {
   
 
   return (
     <>
-      <Header />
-      <Nav />
+      <PagHome/>
     </>
   )
 }
