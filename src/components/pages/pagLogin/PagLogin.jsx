14074cd29ac9779818f4f06b3e0b2b31915e8796
DalisonCPU <dalisonmg@gmail.com>
2024-08-26 13:32:33 -0300

    feat: Trabalhando na página de login

--- src/components/pages/pagLogin/PagLogin.jsx
+++ src/components/pages/pagLogin/PagLogin.jsx
@@ -1,20 +1,26 @@
 import './PagLogin.css';
 import { validarCampo } from '../../../utils/validation-user'
+import Erro from '../../erro/Erro'
 import { useState } from 'react';
 
 export default function PagLogin() {
+  const [erros, setErros] = useState({})
   const [emailINPT, setEmailINPT] = useState("")
   const [senha, setSenha] = useState("")
 
-  function entra() {
+  function entra(form) {
+    form.preventDefault()
+    let listaErros = {}
     let resposta = validarCampo(document.getElementById('email'))
     if(resposta) {
-      alert(resposta)
+      listaErros.email = resposta
     }
     resposta = validarCampo(document.getElementById("senha"))
     if(resposta) {
-      alert(resposta)
+      listaErros.senha = resposta
     }
+
+    setErros(listaErros)
   }
 
 
@@ -30,20 +36,22 @@ export default function PagLogin() {
         <div className='formulario-login'>
 
           <h1 className='titulo'>Entrar</h1>
-          <form>
+          <form onSubmit={entra}>
             <div className='grupo-input'>
               <label htmlFor='email'>Email</label>
               <input type='email' name='email' onChange={(e) => setEmailINPT(e.target.value)} value={emailINPT} id='email' />
+              <Erro mensagem={erros.email} />
             </div>
             <div className='grupo-input'>
               <label htmlFor='senha'>Senha</label>
               <input type='password' name='senha' id='senha' onChange={(e) => setSenha(e.target.value)} value={senha} />
+              <Erro mensagem={erros.senha} />
             </div>
             <div className='grupo-checkbox'>
               <input type='checkbox' id='lembrar' />
               <label htmlFor='lembrar' className='btn-lembrar-de-mim'>Lembrar de mim</label>
             </div>
-            <button className='btn-entrar' onClick={entra}>Entrar</button>
+            <button className='btn-entrar' type='submit'>Entrar</button>
           </form>
           <div className='cadastro'>
             <p>Não possui cadastro?</p>
