c2d6cac170a1f366096994a4222b1b5293fca35b
joao <joaopedrot1912@gmail.com>
2024-08-26 15:08:29 -0300

    refactor: imports sem uso removidos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
 import PagCadeiras from './components/pages/pagCadeiras/PagCadeiras'
 
 function App() {
