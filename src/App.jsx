4eb01e670e8704817f843164c2971ac059023a0d
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-08-26 20:09:16 -0300

    front proto
    a label peso e altura se movem quando trocado para tela maior

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import PagLogin from './components/pages/pagLogin/PagLogin'
-import PagCadastro from './components/pages/pagCadastro/PagCadastro'
+
 import PagErgonomia from './components/pages/pagErgonomia/PagErgonomia'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
   return (
     <>
       
-    <PagCadastro/>
+    <PagErgonomia/>
     </>
   )
 }
