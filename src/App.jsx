3cbd66eed59eb7225c41c13b486119ecd11fd5ce
Joao Pedro <joaopedrot1912@gmail.com>
2024-08-28 14:46:58 -0300

    feat: base da pagina carrinho construida

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import PagLogin from './components/pages/pagLogin/PagLogin'
 import PagCadeiras from './components/pages/pagCadeiras/PagCadeiras'
+import PagCarrinho from './components/pages/pagCarrinho/PagCarrinho'
 
 function App() {
   
 
   return (
     <>
-      <PagCadeiras />
+      <PagCarrinho />
     </>
   )
 }
