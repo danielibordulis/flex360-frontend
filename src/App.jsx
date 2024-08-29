8c7ac1761aa9b452c5761cc452008bf5c1ad6025
Zuma Elma Pereira <zumapereira@hotmail.com>
2024-08-29 09:42:56 -0300

    joao ver  por que o header deformou

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import PagCadeiraIndividual from './components/pages/pagCadeiraIndividual/PagCadeiraIndividual'
+
 import PagAcessorios from './components/pages/pagAcessorios/PagAcessorios'
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <PagCadeiras />
+      <PagAcessorios />
     </>
   )
 }
