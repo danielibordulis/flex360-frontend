986e393d74f9787c64b1ff2a385454e3e4d9e623
joao <joaopedrot1912@gmail.com>
2024-08-29 15:09:27 -0300

    merge: conflitos resolvidos

--- src/pages/pagAcessorios/PagAcessorios.jsx
+++ src/pages/pagAcessorios/PagAcessorios.jsx
@@ -9,39 +9,67 @@ import './PagAcessorios.css';
 export default function PagAcessorios() {
   const produtos = [
     {
+<<<<<<< HEAD
       imagem: '/carregador-acessorios.png',
+=======
+      imagem: 'public/carregador-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Carregador por indução',
       preco: 250.00,
     },
     {
+<<<<<<< HEAD
       imagem: '/pistao-acessorios.png',
+=======
+      imagem: 'public/pistao-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Pistão á gás',
       preco: 280.00,
     },
     {
+<<<<<<< HEAD
       imagem: '/rodinha-acessorios.png',
+=======
+      imagem: 'public/rodinha-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Rodízio em gel c/ capa preta',
       preco: 80.00,
     },
     {
+<<<<<<< HEAD
       imagem: '/monitor-acessorios.png',
+=======
+      imagem: 'public/monitor-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Suporte para monitor',
       preco: 1154.23,
     },
     {
+<<<<<<< HEAD
       imagem: '/notebook-acessorios.png',
+=======
+      imagem: 'public/notebook-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Suporte para notebook',
       preco: 893.00,
     },
     {
+<<<<<<< HEAD
       imagem: '/apoiaPes-acessorios.png',
+=======
+      imagem: 'public/apoiaPes-acessorios.png',
+>>>>>>> 52dc0dd (produtos e descricao)
       descricao: 'Apoia pés ergonômico',
       preco: 230.23,
     },
   ];
 
   return (
+<<<<<<< HEAD
     <>
+=======
+    <div className="pag-acessorios">
+>>>>>>> 52dc0dd (produtos e descricao)
       <Header/>
       <Nav />
       <section className="body">
@@ -54,6 +82,10 @@ export default function PagAcessorios() {
           />
         ))}
       </section>
+<<<<<<< HEAD
     </>
+=======
+    </div>
+>>>>>>> 52dc0dd (produtos e descricao)
   );
 }
