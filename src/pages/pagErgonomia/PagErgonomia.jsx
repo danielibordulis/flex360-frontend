0f759c592d704210cc2dd62b599098f90f0face9
joao <joaopedrot1912@gmail.com>
2024-09-03 13:46:12 -0300

    page

---
+++ src/pages/pagErgonomia/PagErgonomia.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import PagErgonomia from "./components/pages/pagErgonomia/PagErgonomia"
+
+function App() {
+  
+  return (
+    <>
+      <PagErgonomia />
+      
+    </>
+  )
+}
+
+export default App
