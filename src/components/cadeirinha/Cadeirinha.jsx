a58a91cd53ad4b386358506ddba532539d0f217d
Danieli <danielibordulis@gmail.com>
2024-09-03 16:48:27 -0300

    feito componente cadeirinha

---
+++ src/components/cadeirinha/Cadeirinha.jsx
@@ -0,0 +1,19 @@
+import './Cadeirinha.css'
+
+function Cadeirinha() {
+  return (
+    <>
+      <div className='container-cadeirinha'>
+            <div className='div-img'>
+                <img src="./cadeirinha.png" alt="cadeirinha" />
+            </div>
+            <div className='texto'>
+                <p className='nomeCadeira'>Cadeira led Black n white</p>
+                <p className='preço'>R$ 1.999,00</p>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default Cadeirinha
