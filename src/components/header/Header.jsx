1992b6a6bb17a1d8b25892ef092d193b31e72301
Joao Pedro <joaopedrot1912@gmail.com>
2024-09-03 20:01:39 -0300

    refactor: componente nav agora é usando dentro do componente header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import "./Header.css"
+import Nav from '../nav/Nav'
 
 function Header() {
   return (
-    <header>
-        <div className='container-logo'>
-            <img className='imagem-logo' alt='Logo' src='./logo-grande.png'/>
-        </div>
-        <div className='container-search-input'>
-            <input placeholder='O que você procura?...'/>
-        </div>
-        <div className='container-botao-nav'>
-            <button>
-                <img alt='Imagem do botão perfil' src='./perfil.png' className='botao-perfil'/>
-            </button>
-            <button>
-                <img alt='Imagem do botão carrinho' src='./carrinho.png' className='botao-carrinho'/>
-            </button>
-        </div>
-    </header>
+    <>
+        <header>
+            <div className='container-logo'>
+                <img className='imagem-logo' alt='Logo' src='./logo-grande.png'/>
+            </div>
+            <div className='container-search-input'>
+                <input placeholder='O que você procura?...'/>
+            </div>
+            <div className='container-botao-nav'>
+                <button>
+                    <img alt='Imagem do botão perfil' src='./perfil.png' className='botao-perfil'/>
+                </button>
+                <button>
+                    <img alt='Imagem do botão carrinho' src='./carrinho.png' className='botao-carrinho'/>
+                </button>
+            </div>
+        </header>
+        <Nav />
+    </>
   )
 }
 
