21c053948fae257645759d71c07aa4393e989f16
Joao Pedro <joaopedrot1912@gmail.com>
2024-10-25 18:44:36 -0300

    refactor: base do projeto alterado

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'http://flex360-front-ae8fh.s3-website-us-east-1.amazonaws.com/'
 })
